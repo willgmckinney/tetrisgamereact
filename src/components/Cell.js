e989a2204090222c8048498a634805b1d4ec3b89
William Mckinney <williammckinney@Williams-MacBook-Pro.local>
2020-03-14 20:50:27 -0400

    nearly finished with tetris project

---
+++ src/components/Cell.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {StyledCell} from './styles/StyledCell';
+import {TETROMINOS} from '../tetrominos';
+
+export default function Cell({type}) {
+    return (
+        <StyledCell type={type} color={TETROMINOS[type].color}/>
+    )
+}
